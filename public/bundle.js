e43cd31ebe824100a4d46e4a32ef09f50e30a6d1
jaroslaw gryko <jaroslaw.gryko@gmail.com>
2017-01-12 22:19:18 +0100

    Rename example page

--- public/bundle.js
+++ public/bundle.js
@@ -26648,19 +26648,20 @@
 
 	var React = __webpack_require__(1);
 
-	//var Examples = React.createClass({
-	//    render: function(){
-	//        return (
-	//            <h3>Examples component.</h3>
-	//        );
-	//    }
-	//});
-
 	var Examples = function Examples(props) {
 	    return React.createElement(
-	        'h3',
+	        'div',
 	        null,
-	        'Examples component.'
+	        React.createElement(
+	            'h3',
+	            null,
+	            'Examples'
+	        ),
+	        React.createElement(
+	            'p',
+	            null,
+	            'Welcome to examples page.'
+	        )
 	    );
 	};
 
